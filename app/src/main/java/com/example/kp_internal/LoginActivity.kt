9ec93578373adb60df31be56a65cec16b3c5738f
lvsnflwrs <149216118+Lvsnflwrs@users.noreply.github.com>
2025-04-21 19:50:01 +0700

    menambahkan page login dan dashboard kosong

--- app/src/main/java/com/example/kp_internal/LoginActivity.kt
+++ app/src/main/java/com/example/kp_internal/LoginActivity.kt
@@ -2,28 +2,28 @@ package com.example.kp_internal
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnclickLogin: Button = findViewById(R.id.btn_login)
 
-        val btnclickLogout: Button = findViewById(R.id.btn_logout)
-
-        btnclickLogout.setOnClickListener{
-            val bukaLogin = Intent(this, LoginActivity::class.java)
-            startActivity(bukaLogin)
+        btnclickLogin.setOnClickListener{
+            val bukaDashbaord = Intent(this, MainActivity::class.java)
+            startActivity(bukaDashbaord)
         }
+
     }
 }
